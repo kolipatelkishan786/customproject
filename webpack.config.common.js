85b8b63c8a096403fafbad6df6d07641175b33c7
Kishan Patel <kolipatelkishan786@gmail.com>
2019-03-25 15:03:37 +0530

    386 Finishing  Using the Development Workflow.

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,7 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/app.main.ts",
+    entry: "./src/app/main.ts",
     resolve: {
         extensions: ['.js', '.ts ']
     },
