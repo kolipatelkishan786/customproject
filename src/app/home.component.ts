d59c73b6c10e105f1b6f5b23579b24a7d4436472
Kishan Patel <kolipatelkishan786@gmail.com>
2019-03-25 11:32:41 +0530

    382 Filling the Project Files with Some Life.

---
+++ src/app/home.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    template: '<h1>The Home Page</h1>'
+})
+export class HomeComponent {
+    
+}
---
+++ src/app/users/users.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from "@angular/core";
+import {UsersComponent} from "./users.component";
+import {UsersRoutingModule} from "./users-routing.module";
+import {CommonModule} from "@angular/common";
+
+@NgModule({
+    declarations: [
+        UsersComponent,
+    ],
+    imports: [
+        CommonModule,
+        UsersRoutingModule
+    ],
+    providers: [],
+    bootstrap: []
+})
+export class UsersModule {
+
+}
